b286cb97c2e39e040792a4e8455b27cdd2be0124
Kevin <vukevin0212@gmail.com>
2023-01-18 14:48:50 -0500

    Changed banner to linear gradient Styled icons in the footer and surrounded them with anchor tags Added Basic description to event texter

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var 
+
+function copyText(element){
+   text = document.getElementById(element);
+   text.select();
+   text.setSelectionRange(0, 99999);
+
+   navigator.clipboard.writeText(text.value);
+
+   alert("Copied: " + text.value + " to clipboard");
+
+}
